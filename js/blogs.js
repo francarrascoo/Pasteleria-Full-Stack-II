00e8b898f5040212cbdd138e0afcadcb6775f1aa
MAHS-coding <mahscoding@gmail.com>
2025-09-15 03:51:46 -0300

    Arreglo pagina de blog

--- js/blogs.js
+++ js/blogs.js
@@ -8,7 +8,7 @@ const POSTS = [
         titulo: "Torta de Chocolate: 5 trucos para que quede húmeda",
         resumen:
             "Aprende a preparar una torta de chocolate perfecta: balance de cacao, tiempo de horneado y almíbares.",
-        imagen: "/img/blog/torta_chocolate.jpg",
+        imagen: "/img/blog/TortaChocolate.jpg",
         fecha: "2025-09-01",
         autor: "Pastelería Mil Sabores",
         link: "/pages/detalle.html?id=torta-chocolate&type=blog"
@@ -18,7 +18,7 @@ const POSTS = [
         titulo: "Rellenos frescos de primavera",
         resumen:
             "Tres combinaciones de frutas y cremas que sorprenden, con tips de conservación y armado.",
-        imagen: "/img/blog/rellenos_primavera.jpg",
+        imagen: "/img/blog/RellenoDulce.jpg",
         fecha: "2025-08-25",
         autor: "Equipo de Pastelería",
         link: "/pages/detalle.html?id=rellenos-primavera&type=blog"
@@ -28,7 +28,7 @@ const POSTS = [
         titulo: "Postres sin azúcar: guía de endulzantes",
         resumen:
             "Stevia, eritritol y más: cómo reemplazar sin perder textura ni sabor.",
-        imagen: "/img/blog/sin_azucar.jpg",
+        imagen: "/img/blog/SinAzucar.jpg",
         fecha: "2025-08-10",
         autor: "Francisca",
         link: "/pages/detalle.html?id=sin-azucar&type=blog"
